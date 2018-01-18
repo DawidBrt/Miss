cb442871bb0d8687b6abe5a609d4c9c8504890fc
Didans <dawid.ns@hotmail.com>
2018-01-18 10:47:51 +0100

    Final version

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,16 @@ import simulation.Simulation;
 
 public class Test {
 	public static void main(String[] args) {
-		Simulation simulation = new Simulation(args[0], args[1]);
+		Simulation simulation;
+		if(args.length<2){
+			System.out.println("Nie podano odpowiedniej liczby argumentów");
+			System.out.println("Ustawiono argumenty na domyślne");
+			System.out.println("Log: sim.log | Obraz: img1.png");
+			simulation = new Simulation("sim.log","img1.png");
+		}
+		else {
+			simulation = new Simulation(args[0], args[1]);
+		}
 		simulation.simualtion();
 	}
 }
