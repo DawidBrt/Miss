df9c3a61aa519491f9f5ad07367b968d82ec8f87
Didans <dawid.ns@hotmail.com>
2018-01-18 13:26:12 +0100

    Final version

--- src/Test.java
+++ src/Test.java
@@ -6,8 +6,8 @@ public class Test {
 		if(args.length<2){
 			System.out.println("Nie podano odpowiedniej liczby argumentów");
 			System.out.println("Ustawiono argumenty na domyślne");
-			System.out.println("Log: sim.log | Obraz: img1.png");
-			simulation = new Simulation("sim.log","img1.png");
+			System.out.println("Log: log/sim.log | Obraz: img/img1.png");
+			simulation = new Simulation("log/sim.log","img/img1.png");
 		}
 		else {
 			simulation = new Simulation(args[0], args[1]);
